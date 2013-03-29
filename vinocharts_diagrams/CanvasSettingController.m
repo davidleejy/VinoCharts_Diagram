b5b1a9c9ea8aaa8342a68a7cca69948f5eef3dda
Lee Jian Yi David <ljydavid@soctf-imac-028.comp.nus.edu.sg>
2013-03-29 17:43:09 +0800

    improved prev commit
    Signed-off-by: Lee Jian Yi David <ljydavid@soctf-imac-028.comp.nus.edu.sg>

--- vinocharts_diagrams/CanvasSettingController.m
+++ vinocharts_diagrams/CanvasSettingController.m
@@ -10,6 +10,8 @@
 
 #import "CanvasSettingController.h"
 
+#import "Constants.h"
+
 @implementation CanvasSettingController
 
 
@@ -31,10 +33,15 @@
     [f setNumberStyle:NSNumberFormatterDecimalStyle];
     NSNumber *w = [f numberFromString:_widthDisplay.text];
     NSNumber *h = [f numberFromString:_heightDisplay.text];
+    double w_dbl = [w doubleValue];
+    double h_dbl = [h doubleValue];
     
     if (DEBUG) {
         NSLog(@"CanvasSettingController okButton width %@ height %@",w,h);
     }
+    
+    double note_default_width = NOTE_DEFAULT_WIDTH;
+    double note_default_height = NOTE_DEFAULT_HEIGHT;
     
     if (w == nil || h == nil) {
         UIAlertView *alert = [[UIAlertView alloc]initWithTitle:[NSString stringWithFormat:@"Width and height must be numbers."]
@@ -44,6 +51,14 @@
                                              otherButtonTitles:nil];
         [alert show];
         [alert performSelector:@selector(dismissAnimated:) withObject:(BOOL)NO afterDelay:1.0f];
+    }
+    else if (w_dbl <= note_default_width|| h_dbl <= note_default_height) {
+        UIAlertView *alert = [[UIAlertView alloc]initWithTitle:[NSString stringWithFormat:@"Width and height must be large enough to accommodate at least 1 note. The default width and height of a note are %d and %d respectively.",NOTE_DEFAULT_WIDTH,NOTE_DEFAULT_HEIGHT]
+                                                       message:nil
+                                                      delegate:self
+                                             cancelButtonTitle:@"OK"
+                                             otherButtonTitles:nil];
+        [alert show];
     }
     else {
         [_delegate CanvasSettingControllerDelegateOkButton:[w doubleValue] :[h doubleValue]];
